b889e49aa99147882c574a6de14893b3f3c98c0b
A1kol <aikolardak@gmail.com>
2026-02-05 11:24:24 +0600

    Aikol. Baza

---
+++ backend/src/main/java/portfolio/studytube/StudyTubeApplication.java
@@ -0,0 +1,13 @@
+package portfolio.studytube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyTubeApplication.class, args);
+    }
+
+}
